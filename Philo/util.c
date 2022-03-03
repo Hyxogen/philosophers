2d0ec863bde9efc0caee2d75095afc6d255d6d32
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-03-03 15:28:23 +0100

    Norminize util.c

--- Philo/util.c
+++ Philo/util.c
@@ -25,35 +25,6 @@
 # endif
 #endif
 
-t_bool
-	fork_new(t_fork *fork)
-{
-	fork->user = NULL;
-	if (pthread_mutex_init(&fork->mtx, NULL))
-		return (FALSE);
-	return (TRUE);
-}
-
-void
-	fork_destroy(t_fork *fork)
-{
-	fork->user = NULL;
-	pthread_mutex_destroy(&fork->mtx);
-}
-
-t_bool
-	philo_new(t_philo *philo, int number, t_philo_attribs *attrib,
-			t_philo	*right) {
-	memset(philo, 0, sizeof(*philo));
-	philo->id = number;
-	philo->attrib = attrib;
-	philo->state = st_start;
-	philo->rfork = &right->lfork;
-	if (!fork_new(&philo->lfork))
-		return (FALSE);
-	return (TRUE);
-}
-
 int
 	philo_usleep(t_philo *philo, long microseconds)
 {
@@ -83,8 +54,8 @@ int
 int
 	ft_usleep(long microseconds)
 {
-	long	now;
-	struct timeval val;
+	long			now;
+	struct timeval	val;
 
 	if (gettimeofday(&val, NULL) <= -1)
 		return (0);
@@ -108,7 +79,7 @@ int
 long
 	philo_get_now(void)
 {
-	struct timeval val;
+	struct timeval	val;
 
 	if (gettimeofday(&val, NULL) <= -1)
 		return (-1);
