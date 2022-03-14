d29d6bc5d8e400deafcdc67962de89348053fdf2
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-03-14 09:31:51 +0100

    Norminize setup.c

--- Philo/setup.c
+++ Philo/setup.c
@@ -4,32 +4,33 @@
 int
 	ph_fork_new(t_fork *fork)
 {
-		if (pthread_mutex_init(&fork->mtx, NULL))
-				return (-1);
-		fork->user = NULL;
-		return (0);
+	if (pthread_mutex_init(&fork->mtx, NULL))
+		return (-1);
+	fork->user = NULL;
+	return (0);
 }
 
-int		ph_philo_new(t_philo *philo, int id, t_philo_attr *attr, t_philo *right)
+int
+	ph_philo_new(t_philo *philo, int id, t_philo_attr *attr, t_philo *right)
 {
-		memset(philo, 0, sizeof(*philo));
-		philo->id = id;
-		philo->attr = attr;
-		ph_fork_new(&philo->lfork);
-		if (right)
-				philo->rfork = &right->lfork;
-		return (0);
+	memset(philo, 0, sizeof(*philo));
+	philo->id = id;
+	philo->attr = attr;
+	ph_fork_new(&philo->lfork);
+	if (right)
+			philo->rfork = &right->lfork;
+	return (0);
 }
 
 int
 	ph_app_new(t_app *app, t_philo_attr *attr)
 {
-		if (pthread_mutex_init(&app->global_mtx, NULL))
-			return (-1);
-		app->state = 0;
-		app->start = 0;
-		app->attr = attr;
-		return (0);
+	if (pthread_mutex_init(&app->global_mtx, NULL))
+		return (-1);
+	app->state = 0;
+	app->start = 0;
+	app->attr = attr;
+	return (0);
 }
 
 int
@@ -50,4 +51,3 @@ int
 	attr->sleep_time *= 1000;
 	return (success);
 }
-
